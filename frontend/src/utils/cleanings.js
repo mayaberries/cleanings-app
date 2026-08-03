9f1c27ee9b0b8bbe40cd783507f5cfa15dfcb226
Maya Morales <maymorales@proton.me>
2026-08-03 14:22:03 -0600

    removed-frontend-proj

--- frontend/src/utils/cleanings.js
+++
@@ -1,5 +0,0 @@
-export const userIsOwnerOfCleaningJob = (cleaning, user) => {
-  if (cleaning?.owner?.id === user?.id) return true;
-  if (cleaning?.owner === user?.id) return true;
-  return false;
-};
