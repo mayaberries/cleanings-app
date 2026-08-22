1c85df082644fee80e0c03a4a73205b3b8315aef
Maya Morales <maymorales@proton.me>
2026-08-21 18:05:11 -0600

    adds-vue-and-nanostores-to-admin

--- frontend/admin/astro.config.mjs
+++ frontend/admin/astro.config.mjs
@@ -2,10 +2,15 @@ import { defineConfig } from 'astro/config';
 import node from '@astrojs/node';
 import tailwindcss from '@tailwindcss/vite';
 
+import vue from '@astrojs/vue';
+
 export default defineConfig({
   output: 'server',
   adapter: node({ mode: 'standalone' }),
+
   vite: {
     plugins: [tailwindcss()],
   },
+
+  integrations: [vue()],
 });
